fcb76c711baeebdb6daf2bee2d5de1e6906f42db
Moredy <matheusmarques1902@gmail.com>
2022-06-17 23:49:25 -0300

    t

--- main.py
+++ main.py
@@ -1,32 +1,37 @@
 import fastapi.middleware.cors as _cors
-
+import firebase_admin
 from typing import Union
 from fastapi import FastAPI
+from microservices import FoodService
+from fastapi.responses import RedirectResponse
+
+databaseURL = 'https://appdobandeco-default-rtdb.firebaseio.com';
+
+cred_obj = firebase_admin.credentials.Certificate('./firebase_credentials.json')
+firebase_admin.initialize_app(cred_obj, {'databaseURL': databaseURL})
 
 app = FastAPI()
 
 '''
 Configuração de CORS
-
+'''
 origins = [
-    "http://127.0.0.1:8080",
+    "http://127.0.0.1:19002",
 
 ]
 
 app.add_middleware(
     _cors.CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
 )
 
-'''
+
+
+app.include_router(FoodService.router)
+
 @app.get("/")
 def read_root():
-    return {"oi": "oi"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+    return RedirectResponse("/redoc")
---
+++ microservices/FoodService.py
@@ -0,0 +1,96 @@
+from warnings import catch_warnings
+from firebase_admin import db
+from fastapi import APIRouter, Depends, HTTPException
+from pydantic import BaseModel
+
+router = APIRouter()
+
+
+class DefaultResponseModel(BaseModel):
+    statusCode: int
+    message:str
+
+
+@router.put(
+    "/foodService/givefoodlike/{food_id}/{user_id}",
+    response_model=DefaultResponseModel
+)
+def give_food_like(food_id: int, user_id: str):
+    userRef = db.reference( 'users/').child(user_id);
+    foodRef = db.reference( 'foods/'+ str(food_id));
+
+    likedFoods = []
+    totalFoodLikes = 0
+
+    try:
+        likedFoods = userRef.get()['likedFoods']
+    except:
+        print("--Esse usuário ainda não deu like em nenhuma comida, adicionando primeiro registro")
+    
+
+    #Caso não tenha dado like na comida
+    if not likedFoods.__contains__(food_id): 
+        likedFoods.append(food_id)
+    else:
+       return {"statusCode": 500, "message": "Este usuário ja deu like nessa comida."}
+
+    userRef.update({
+        'likedFoods' : likedFoods
+    })
+
+    #Adiciona o like na comida
+    try:
+        totalFoodLikes = foodRef.get()['likes'];
+    except:
+        print("Essa comida ainda não recebeu likes, adicionando primeiro registro")
+
+    totalFoodLikes += 1;
+
+    foodRef.update({
+        'likes' : totalFoodLikes
+    })
+
+    return {"statusCode": 200, "message": "Like adicionado com sucesso."}
+
+
+@router.delete(
+    "/foodService/removefoodlike/{food_id}/{user_id}",
+    response_model=DefaultResponseModel
+)
+def remove_food_like(food_id: int, user_id: str):
+    userRef = db.reference( 'users/').child(user_id);
+    foodRef = db.reference( 'foods/'+ str(food_id));
+
+    likedFoods = []
+    totalFoodLikes = 0
+
+    try:
+        likedFoods = userRef.get()['likedFoods']
+    except:
+        return {"statusCode": 500, "message": "Este usuário nunca deu like em nenhuma comida."}
+
+    #Caso não tenha dado like na comida
+    if likedFoods.__contains__(food_id): 
+        likedFoods.remove(food_id)
+    else:
+       return {"statusCode": 500, "message": "Esse usuário não deu like nessa comida."}
+
+    userRef.update({
+        'likedFoods' : likedFoods
+    })
+
+    #Adiciona o like na comida
+    try:
+        totalFoodLikes = foodRef.get()['likes'];
+    except:
+        print("Essa comida ainda não recebeu likes, adicionando primeiro registro")
+
+    totalFoodLikes -= 1;
+
+    foodRef.update({
+        'likes' : totalFoodLikes
+    })
+
+    return {"statusCode": 200, "message": "Like descontado com sucesso."}
+
+
